969dee4bdf072af01dac080dca6d1e09e34195d3
Shashwat Singh <shashwatsingh14@gmail.com>
2017-12-20 17:08:40 +0530

    Update bookData.js

--- data/bookData.js
+++ data/bookData.js
@@ -4,34 +4,25 @@ module.exports = {
     {
         id: 1,
         title: "And then there were none",
-        author: "J K Rowling",
-        publisher: 'Bloomsbury',
-        publishedDate: '2000-03-04',
-        price: 400,
-        genre: 'Fiction',
+        author: "Agatha Christie",
+        publisher: 'Oxford',
+        publishedDate: '2012-03-04',
+        price: 500,
+        genre: 'fiction',
         format: 'Hardcopy'   
     },
 
     {
-        id: 23476,
-        title: "Inferno",
-        author: "Dan Brown",
-        publisher: 'Doubleday',
-        publishedDate: '2013-04-11',
-        price: 150,
-        genre: 'Mystery',
-        format: 'Paperback'
-    },
-
-    {
-        id: 34987,
-        title: "A Million Miles in a Thousand Years",
-        author: "Donald Miller",
-        publisher: 'Thomas Nelson',
-        publishedDate: '2009-11-03',
-        price: 100,
-        genre: 'Romance',
-        format: 'E-Book',
+        id: 2,
+        title: "Killing Floor",
+        author: "Lee Child",
+        publisher: 'Dolphin',
+        publishedDate: '2015-05-11',
+        price: 350,
+        genre: 'mystery',
+        format: 'paperback'
     }
+
+    
   ]
 };
